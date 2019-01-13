149460f65dcbc7df7d32f9b49750708ea98dfa5c
Tomás Fox <tomas.c.fox@gmail.com>
2019-01-13 15:26:06 -0300

    v0.3.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='graphene-django-optimizer',
-    version='0.3.5',
+    version='0.3.6',
     author='Tomás Fox',
     author_email='tomas.c.fox@gmail.com',
     description='Optimize database access inside graphene queries.',

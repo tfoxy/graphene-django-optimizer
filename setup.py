760676ea7ab22e9a4a2687a79ed410cba9226e29
Tomás Fox <tomas.c.fox@gmail.com>
2020-09-14 08:13:49 -0300

    Bump version to v0.7.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='graphene-django-optimizer',
-    version='0.6.3',
+    version='0.7.0',
     author='Tomás Fox',
     author_email='tomas.c.fox@gmail.com',
     description='Optimize database access inside graphene queries.',
